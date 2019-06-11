733c8da9db09564baf2de57905d06a7a4fd2427e
AnnaClaire Marley <annaclairemarley@gmail.com>
2019-06-10 18:19:02 -0700

    more skeleton

---
+++ R/calc_crop_harvest.R
@@ -0,0 +1,22 @@
+#' calc_crop_harvest
+#'
+#' Calculates the crop changes in harvest using temperature data and crop parameters. Disclaimer: this is a made up function.
+#'
+#' @param crop_temp # local temperature dataset
+#' @param mean_temp # enter the mean temp user wants to use
+#' @param crop_parameter # crop paramter unique to the crop you want to use
+#'
+#' @return # lists mean temperatures per year first then harvest value per year
+#' @export
+#'
+#' @examples
+calc_crop_harvest = function(crop_temp, mean_temp, crop_parameter) {
+  # calculates the mean temperature for each year across locations
+  threshold = apply(crop_temp, MARGIN = 1, FUN = mean)
+  # use this information to calculate harvest value using parameters set by user
+  harvest = (mean_temp - threshold)*crop_parameter
+
+  return(list(thresholds = threshold,
+         harvests = harvest))
+
+}
--- tests/testthat/test-calc_crop_harvest.R
+++ tests/testthat/test-calc_crop_harvest.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+
+crop_test = sample(1:1, 500, replace = T)
+dates = seq(from = 1920, to = 2010, by = 10)
+locations = c("SB", "LA", "SF","SLO","SD")
+
+ca_test <- matrix(crop_test, nrow = 10, ncol = 5,
+                  dimnames = list(c(dates), c(locations)))
+
+
+test_that("crops_equal_one", {
+  expect_that(calc_crop_harvest(crop_test, 2, 1)$harvests[1], equals(2))
+
+}
+)
