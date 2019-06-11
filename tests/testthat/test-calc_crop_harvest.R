bf4e2c23b9a2a2c611a9ac7bc0b541055c600d8b
AnnaClaire Marley <annaclairemarley@gmail.com>
2019-06-11 10:47:44 -0700

    added to testing

--- tests/testthat/test-calc_crop_harvest.R
+++ tests/testthat/test-calc_crop_harvest.R
@@ -1,14 +1,13 @@
 
-crop_test = sample(1:1, 500, replace = T)
-dates = seq(from = 1920, to = 2010, by = 10)
-locations = c("SB", "LA", "SF","SLO","SD")
-
-ca_test <- matrix(crop_test, nrow = 10, ncol = 5,
-                  dimnames = list(c(dates), c(locations)))
-
-
 test_that("crops_equal_one", {
-  expect_that(calc_crop_harvest(crop_test, 2, 1)$harvests[1], equals(2))
+  #fake dataset:
+  crop_test = sample(1:1, 500, replace = T)
+  dates = seq(from = 1920, to = 2010, by = 10)
+  locations = c("SB", "LA", "SF","SLO","SD")
+  ca_test <- matrix(crop_test, nrow = 10, ncol = 5,
+                    dimnames = list(c(dates), c(locations)))
+
+  expect_that(calc_crop_harvest(ca_test, 2, 1)$harvests[1], equals(c('1920' = 1)))
 
 }
 )
--- tests/testthat/test-calc_extreme_rain_anom.R
+++ tests/testthat/test-calc_extreme_rain_anom.R
@@ -1,15 +1,14 @@
-rain_test_data =
-  as.data.frame(
-    cbind(
-      Date = c(1:4),
-      Value = c(1:4),
-      Anomaly = c(1:4)
-    )
-  )
-
+# this is the test that doesn't say the function = 4 even though when you run it through the function says it = 4
 
 test_that("please_work", {
-expect_that(calc_extreme_rain_anom(rain_test_data)$max_anom, equals(4))
+  rain_test_data =
+    data.frame(
+        Date = c(1:4),
+        Value = c(1:4),
+        Anomaly = c(1:4)
+    )
+
+  expect_true(as.integer(calc_extreme_rain_anom(rain_test_data)$max_anom) == 4)
 
 }
 )
