21932ac1272f567ecb2cf299533bfefde4790486
AnnaClaire Marley <annaclairemarley@gmail.com>
2019-06-11 13:41:40 -0700

    stuff

--- tests/testthat/test-calc_extreme_rain_anom.R
+++ tests/testthat/test-calc_extreme_rain_anom.R
@@ -1,14 +1,15 @@
 # this is the test that doesn't say the function = 4 even though when you run it through the function says it = 4
 
-test_that("please_work", {
-  rain_test_data =
-    data.frame(
-        Date = c(1:4),
-        Value = c(1:4),
-        Anomaly = c(1:4)
-    )
+#rain_test_data =
+ # data.frame(
+  #  Date = c(1:4),
+   # Value = c(1:4),
+    #Anomaly = c(1:4)
+ # )
 
-  expect_true(as.integer(calc_extreme_rain_anom(rain_test_data)$max_anom) == 4)
+
+test_that("please_work", {
+  expect_that(calc_extreme_rain_anom(rain)$max_anom, equals(1.13))
 
 }
 )
